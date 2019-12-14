0d18c4d931b11bd8aa660efb326e39bbe991f2e6
Anant Jain <anant90@gmail.com>
2019-12-13 16:55:44 -0800

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -4,7 +4,7 @@ import string
 
 from django.conf import settings
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import login, logout
 from rauth import OAuth2Service, OAuth2Session
 
